3d6f7671f97f675f1256e1c714d336dc6fb25e16
Sik1995 <yassinejaidane0@gmail.com>
2024-08-11 16:06:10 +0100

    Add files via upload

---
+++ nc-chat/fxmanifest.lua
@@ -0,0 +1,54 @@
+shared_script '@pablo/shared_fg-obfuscated.lua'
+shared_script '@pablo/ai_module_fg-obfuscated.lua'
+-- $$\   $$\  $$$$$$\  $$\   $$\ $$\   $$\ $$$$$$$\  
+-- $$$\  $$ |$$  __$$\ $$ |  $$ |$$ |  $$ |$$  __$$\ 
+-- $$$$\ $$ |$$ /  \__|$$ |  $$ |$$ |  $$ |$$ |  $$ |
+-- $$ $$\$$ |$$ |      $$$$$$$$ |$$ |  $$ |$$$$$$$\ |
+-- $$ \$$$$ |$$ |      $$  __$$ |$$ |  $$ |$$  __$$\ 
+-- $$ |\$$$ |$$ |  $$\ $$ |  $$ |$$ |  $$ |$$ |  $$ |
+-- $$ | \$$ |\$$$$$$  |$$ |  $$ |\$$$$$$  |$$$$$$$  |
+-- \__|  \__| \______/ \__|  \__| \______/ \_______/  
+-- discord.gg/sgx & Patreon.com/NCHub
+--[[ FX Information ]]
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+--[[ Resource Information ]]
+name "nc-chat"
+author "NCHub"
+
+--[[ Manifest ]]
+shared_script "shared/**/*"
+
+client_scripts {
+	"client/variables.lua",
+	"client/functions.lua",
+	"client/events.lua",
+	"client/commands.lua",
+	"client/nui.lua",
+	"client/threads.lua",
+}
+
+server_scripts {
+	"server/variables.lua",
+	"server/functions.lua",
+	"server/commands.lua",
+	"server/events.lua",
+}
+
+ui_page "ui/build/index.html"
+
+files {
+	"ui/build/index.html",
+	"ui/build/**/*",
+}
+
+escrow_ignore {
+	"shared/**/*",
+	"client/**/*",
+	"server/**/*",
+	"ui/**/*"
+}
+
+dependency '/assetpacks'
